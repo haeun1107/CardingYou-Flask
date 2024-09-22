015584125cd36e063fa11ebfd55314aec400547f
haeun1107 <haeun9394@gmail.com>
2024-09-22 18:22:16 +0900

    REFACTOR: 환경 변수 설정

--- app.py
+++ app.py
@@ -11,7 +11,19 @@ import random
 load_dotenv()
 
 # firebase 연동 초기화
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate({
+  "type": os.getenv("TYPE"),
+  "project_id": os.getenv("PROJECT_ID"),
+  "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+  "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),  # 키의 줄바꿈 처리
+  "client_email": os.getenv("CLIENT_EMAIL"),
+  "client_id": os.getenv("CLIENT_ID"),
+  "auth_uri": os.getenv("AUTH_URI"),
+  "token_uri": os.getenv("TOKEN_URI"),
+  "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_CERT_URL"),
+  "client_x509_cert_url": os.getenv("CLIENT_CERT_URL")
+})
+
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
